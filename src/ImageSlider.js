2379d580df66ce2c5ef44206527d0939c0242f51
github username <github email address>
2021-04-26 13:50:54 -0500

    commit tree 951617f3c1d9679f94756e699f111142f67f7924

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -2,8 +2,8 @@
 import React, { Component } from 'react'
 
 export default class ImageSlider extends Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
         this.state = {
             currentSlideIndex: 0
         }
